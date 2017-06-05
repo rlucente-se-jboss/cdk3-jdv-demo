3b10106c08d3a596823698b9f9c066bbc236d3f3
rlucente <rlucente@redhat.com>
2017-06-05 13:57:10 -0400

    delete redundant apicast

--- 3scale-demo/setup-3scale.sh
+++ 3scale-demo/setup-3scale.sh
@@ -12,9 +12,14 @@ oc new-project 3scale-amp
 
 oc new-app --file amp.yml --param WILDCARD_DOMAIN=amp.${IP_ADDR}.nip.io --param ADMIN_PASSWORD=3scaleUser
 
+sleep 5
+
+oc delete dc/apicast-production
+oc delete rc/apicast-production-1
+oc delete pod/apicast-production-1-deploy
+
 echo
 echo "This will take awhile for all the pods to spin up.  Once they're"
 echo "all running, browse to https://3scale-admin.amp.${IP_ADDR}.nip.io"
 echo "and login using the admin/3scaleUser credentials."
 echo
-
