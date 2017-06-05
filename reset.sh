fe476dfceac752667ca1ca304c78f2d69d1efad6
rlucente <rlucente@redhat.com>
2017-06-05 13:59:59 -0400

    actually reset the demos

--- reset.sh
+++ reset.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-minishift stop
-minishift delete
+IP_ADDR=$(minishift ip)
+
+oc login https://${IP_ADDR}:8443 -u developer
+
+oc delete all --all -n 3scale-amp
+oc delete project 3scale-amp
+
+oc delete all --all -n demo
+oc delete project demo
+
+oc delete all --all -n test
+oc delete project test
 
 pushd `dirname $0` &> /dev/null
     rm -fr library jdv-demo/datasources.properties jdv-demo/jgroups.jceks jdv-demo/keystore.jks 3scale-demo/amp.yml
 popd &> /dev/null
+
+echo
+echo Make sure to clean up the docker images
+echo
 
